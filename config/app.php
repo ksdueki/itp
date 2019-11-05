35932a9f48589ca53f6fca3277d02427e52dd018
ueki <ueki@aok-net.com>
2019-11-05 18:06:59 +0900

    不要になったlibの削除

--- config/app.php
+++ config/app.php
@@ -175,10 +175,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
-        // OpenAPI用（Swagger）
-        // https://vyuldashev.github.io/laravel-openapi/#installation
-        Vyuldashev\LaravelOpenApi\OpenApiServiceProvider::class,
     ],
 
     /*
