f5c07afd151c8fff14fd676f9c3a476535036f1e
ueki <ueki@aok-net.com>
2019-11-05 13:57:21 +0900

    シードデータの追加

--- database/seeds/TownsTableSeeder.php
+++ database/seeds/TownsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Town;
+use Carbon\Carbon;
 
 class TownsTableSeeder extends Seeder
 {
@@ -11,6 +13,11 @@ class TownsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // MySQLから直接いかのコマンドを実行して、シードデータを挿入してください。
+        // マスターを挿入（パス指定の部分は環境によって変化します）
+        // 1. LOAD DATA INFILE "/tmp/towns.csv"  INTO TABLE towns FIELDS TERMINATED BY ","  LINES TERMINATED BY "\n" (name, city_code, code) ;
+        // タイムスタンプを設定
+        // 2. update towns set created_at=NOW()
+        // 3. update towns set updated_at=NOW()
     }
 }
